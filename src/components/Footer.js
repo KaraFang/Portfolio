48da354cdaa80bfd8e7be2fc66be2893e3e6ae29
Kara Fang <kara.fang1@gmail.com>
2023-06-07 02:44:15 -0400

    add Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import styles from '../styles';
+import logo from '../assets/images/logo.png';
+import { socialMedia } from './index';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div className="flex-1 flex flex-col justify-start mr-10">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[100px] h-[100px] object-contain"
+        />
+        <p
+          className={`${styles.paragraph} mt-4 max-w-[310px] text-defaultteal`}
+        >
+          A place to get inspiration.
+        </p>
+      </div>
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+        <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-defaultteal">
+          Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+        </p>
+
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+            <img
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer fill-defaultteal ${
+                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
+              onClick={() => window.open(social.link)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default Footer
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,10 @@ import Home from "../pages/Home";
 import Projects from "../pages/Projects";
 import ContactMe from "../pages/ContactMe";
 import Footer from "./Footer";
+import github from "../assets/images/github.svg";
+import linkedin from "../assets/images/linkedin.svg";
+import instagram from "../assets/images/instagram.svg";
+import twitter from "../assets/images/twitter.svg";
 
 export {
   Navbar,
@@ -25,4 +29,27 @@ export const navLinks = [
       id: "contactMe",
       title: "Contact Me",
     },
+  ];
+
+  export const socialMedia = [
+    {
+      id: "social-media-1",
+      icon: github,
+      link: "https://github.com/KaraFang",
+    },
+    {
+        id: "social-media-2",
+        icon: linkedin,
+        link: "https://www.linkedin.com/in/kara-fang-6202b6167/",
+    },
+    {
+      id: "social-media-3",
+      icon: instagram,
+      link: "https://www.instagram.com/kara_180/",
+    },
+    // {
+    //   id: "social-media-4",
+    //   icon: twitter,
+    //   link: "https://www.twitter.com/",
+    // },
   ];
