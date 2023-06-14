09ebc21afdba647631597b0a5f70dcf1a391dff2
Kara Fang <kara.fang1@gmail.com>
2023-06-14 00:33:39 -0400

    add github link to project list

--- src/projects.js
+++ src/projects.js
@@ -3,10 +3,12 @@ export const projects = [
     id: 1,
     name: 'PORTFOLIO',
     link: 'https://www.karafang.com/',
+    github: 'https://github.com/KaraFang/Portfolio',
   },
   {
     id: 2,
     name: 'FITCOACH',
-    link: '123'
+    link: '123',
+    github: '';
   },
 ];
