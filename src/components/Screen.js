d663b105a5332fbaf539f6743d31d9697912ee92
Kara Fang <kara.fang1@gmail.com>
2023-07-06 00:44:44 -0400

    add condition to show project links

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,32 +3,30 @@ import { FaRegLightbulb, FaLink, FaGithub } from 'react-icons/fa';
 
 const Screen = ({ name, bgimg, website, github }) => {
   return (
-    <section
-      id="gallery-screens"
-    >
-      <div className="screen w-[280px] z-[4] hover:scale-110"
-      >
-        <div className="screen-image bg-cover bg-center" style={{ backgroundImage: `url(${bgimg})` }}></div>
+    <section id="gallery-screens">
+      <div className="screen w-[280px] z-[4] hover:scale-110">
+        <div
+          className="screen-image bg-cover bg-center"
+          style={{ backgroundImage: `url(${bgimg})` }}
+        ></div>
         <div className="screen-overlay"></div>
         <div className="screen-content">
-          <FaRegLightbulb className="screen-icon"/>
+          <FaRegLightbulb className="screen-icon" />
           <div className="screen-user">
-            <span className="name" data-value='name'>
+            <span className="name" data-value="name">
               {name}
             </span>
             <div className="link flex gap-8">
-                <a
-                  href={website}
-                  rel="opener"
-                >
-                  <FaLink className="screen-icon"/>
+              {website ? (
+                <a href={website} rel="opener">
+                  <FaLink className="screen-icon" />
                 </a>
-                <a
-                  href={github}
-                  rel="opener"
-                >
-                  <FaGithub className="screen-icon"/>
+              ) : null}
+              {github ? (
+                <a href={github} rel="opener">
+                  <FaGithub className="screen-icon" />
                 </a>
+              ) : null}
             </div>
           </div>
         </div>
--- src/projects.js
+++ src/projects.js
@@ -10,14 +10,14 @@ export const projects = [
     id: 2,
     name: 'FIT COACH',
     bgimg: 'https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    website: 'https://github.com/KaraFang/Fitcoach',
-    github: '',
+    website: '',
+    github: 'https://github.com/KaraFang/Fitcoach',
   },
   {
     id: 3,
     name: 'DIABLO4 LFG',
     bgimg: 'https://blz-contentstack-images.akamaized.net/v3/assets/blt77f4425de611b362/blte85b0a89fb4504f8/646539b982ac4226f21b97d3/lilith_countdown.jpg?format=webply&quality=90',
-    website: 'https://github.com/KaraFang/LFGApp',
-    github: '',
+    website: '',
+    github: 'https://github.com/KaraFang/LFGApp',
   },
 ];
