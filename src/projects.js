5341a749af06ce46279094c6272155c97c5fd612
Kara Fang <kara.fang1@gmail.com>
2023-07-10 20:32:13 -0400

    update project links

--- src/projects.js
+++ src/projects.js
@@ -1,23 +1,28 @@
+import fitcoach from './assets/images/fitcoach.png';
+import portfolio from './assets/images/portfolio.png';
+
 export const projects = [
   {
     id: 1,
     name: 'PORTFOLIO',
-    bgimg: 'https://images.unsplash.com/photo-1578261984699-4da3d64ce527?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80',
+    // bgimg: 'https://images.unsplash.com/photo-1578261984699-4da3d64ce527?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80',
+    bgimg: portfolio,
     website: 'https://www.karafang.com/',
     github: 'https://github.com/KaraFang/Portfolio',
   },
   {
     id: 2,
     name: 'FIT COACH',
-    bgimg: 'https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-    website: '',
+    // bgimg: 'https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
+    bgimg: fitcoach,
+    website: 'https://fitcoach.vercel.app/',
     github: 'https://github.com/KaraFang/Fitcoach',
   },
-  {
-    id: 3,
-    name: 'DIABLO4 LFG',
-    bgimg: 'https://blz-contentstack-images.akamaized.net/v3/assets/blt77f4425de611b362/blte85b0a89fb4504f8/646539b982ac4226f21b97d3/lilith_countdown.jpg?format=webply&quality=90',
-    website: '',
-    github: 'https://github.com/KaraFang/LFGApp',
-  },
+//   {
+//     id: 3,
+//     name: 'DIABLO4 LFG',
+//     bgimg: 'https://blz-contentstack-images.akamaized.net/v3/assets/blt77f4425de611b362/blte85b0a89fb4504f8/646539b982ac4226f21b97d3/lilith_countdown.jpg?format=webply&quality=90',
+//     website: '',
+//     github: 'https://github.com/KaraFang/LFGApp',
+//   },
 ];
